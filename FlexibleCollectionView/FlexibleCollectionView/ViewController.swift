f6681418892dad41071c53244faddb1b49bc8092
Arsenty Halimovsky <ghost12@yandex.ru>
2023-07-12 05:24:15 +0300

    Add tableViewController

--- FlexibleCollectionView/FlexibleCollectionView/ViewController.swift
+++ FlexibleCollectionView/FlexibleCollectionView/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
+class TableViewController: UIViewController {
+    
+    private let tableView: UITableView = UITableView()
+    private let tableViewCellId = "TableViewCell"
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
+}
 
+// MARK: - Setup UIs
 
+extension TableViewController {
+    
+    private func setupUI() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: tableViewCellId)
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Delegate & DataSource
+
+extension TableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellId) as! TableViewCell
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
 
